2b30c559d581cde580536011820b6c9ca4992b50
lytvyn-dev <frontend.lytvyn@gmail.com>
2022-12-09 11:26:35 +0200

    respinsve

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,7 @@
 import 'core-js';
 import 'regenerator-runtime';
 import recipeView from './views/reciprView';
-import * as modelState from './model.js'
+import * as modelState from './model.js';
 import searchView from './views/searchView';
 import { async } from 'regenerator-runtime';
 import searchResult from './views/searchResult';
@@ -16,103 +16,127 @@ import { TIMEOUT_SEC, MODAl_CLOSE_SEC } from './config';
 // ? }
 
 const controlRecipe = async function () {
-	try {
-		const id = window.location.hash.slice(1);
-		if (!id) return;
-		recipeView.renderSpinner();
-		searchResult.update(modelState.getSearchResultPage());
-		bookmarkView.update(modelState.state.bookmarks);
-		await modelState.loadRecipe(id);
-		recipeView.render(modelState.state.recipe);
-	}
-	catch (err) {
-		recipeView.renderError();
-	}
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+    searchResult.update(modelState.getSearchResultPage());
+    bookmarkView.update(modelState.state.bookmarks);
+    await modelState.loadRecipe(id);
+    recipeView.render(modelState.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
 };
 
-
 const controlSearch = async function () {
-	try {
-		searchResult.renderSpinner();
-		// 1) Get search query
-		const query = searchView.getQuery();
-		if (!query) return;
-		// 2) Load search results
-		await modelState.searchResults(query);
-		searchResult.render(modelState.getSearchResultPage());
-		// 4) Render initial pagination buttons
-		paginationView.render(modelState.state.search);
-	} catch (err) {
-		searchResult.renderError();
-	}
+  try {
+    const sideBarQuery = window.matchMedia('(max-width: 768px)');
+    function initSideBar(e) {
+      if (e.matches) {
+        searchResult._toggleClass();
+        searchResult.addHandlerB();
+      }
+    }
+
+    sideBarQuery.addListener(initSideBar);
+    initSideBar(sideBarQuery);
+
+    // searchResult._toggleClass();
+    searchResult.renderSpinner();
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await modelState.searchResults(query);
+    searchResult.render(modelState.getSearchResultPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(modelState.state.search);
+
+    // // searchResult._toggleClass();
+    // searchResult.renderSpinner();
+    // // 1) Get search query
+    // const query = searchView.getQuery();
+    // if (!query) return;
+
+    // // 2) Load search results
+    // await modelState.searchResults(query);
+    // searchResult.render(modelState.getSearchResultPage());
+
+    // // 4) Render initial pagination buttons
+    // paginationView.render(modelState.state.search);
+  } catch (err) {
+    searchResult.renderError();
+  }
 };
 
 // Pagination
 const controlPaganation = function (goToPage) {
-	// Render html of results from x to y
-	searchResult.render(modelState.getSearchResultPage(goToPage));
-	paginationView.render(modelState.state.search);
-	searchResult.update(modelState.getSearchResultPage());
-}
+  // Render html of results from x to y
+  searchResult.render(modelState.getSearchResultPage(goToPage));
+  paginationView.render(modelState.state.search);
+  searchResult.update(modelState.getSearchResultPage());
+};
 
 const controlServings = function (newServings) {
-	modelState.updateServings(newServings);
-	recipeView.update(modelState.state.recipe);
-}
+  modelState.updateServings(newServings);
+  recipeView.update(modelState.state.recipe);
+};
 
 const controlAddBookmark = function () {
-	//Add bookmark
-	if ((!modelState.state.recipe.bookmarked)) {
-		modelState.addBookMark(modelState.state.recipe);
-		// Remove bookmark
-	} else modelState.deleteBookmark(modelState.state.recipe.id);
+  //Add bookmark
+  if (!modelState.state.recipe.bookmarked) {
+    modelState.addBookMark(modelState.state.recipe);
+    // Remove bookmark
+  } else modelState.deleteBookmark(modelState.state.recipe.id);
 
-	// 2) Update recipe view
-	recipeView.update(modelState.state.recipe);
-	// 3) Render bookmarks
-	bookmarkView.render(modelState.state.bookmarks);
-}
+  // 2) Update recipe view
+  recipeView.update(modelState.state.recipe);
+  // 3) Render bookmarks
+  bookmarkView.render(modelState.state.bookmarks);
+};
 
 const conrolBookmark = function () {
-	bookmarkView.render(modelState.state.bookmarks);
-}
+  bookmarkView.render(modelState.state.bookmarks);
+};
 
 const controlUpload = async function (newRecipe) {
-	try {
-		//render spinner
-		addRecipe.renderSpinner();
+  try {
+    //render spinner
+    addRecipe.renderSpinner();
 
-		await modelState.uploadRecipe(newRecipe);
+    await modelState.uploadRecipe(newRecipe);
 
-		// Add succses message
-		addRecipe.renderMessage()
+    // Add succses message
+    addRecipe.renderMessage();
 
-		//close modal window
-		setTimeout(function () {
-			addRecipe._toggleWindow();
-		}, MODAl_CLOSE_SEC * 1000);
+    //close modal window
+    setTimeout(function () {
+      addRecipe._toggleWindow();
+    }, MODAl_CLOSE_SEC * 1000);
 
-		window.history.pushState(null, '', `#${modelState.state.recipe.id}`);
+    window.history.pushState(null, '', `#${modelState.state.recipe.id}`);
 
-		// render Bookmark
-		bookmarkView.render(modelState.state.bookmarks);
+    // render Bookmark
+    bookmarkView.render(modelState.state.bookmarks);
 
-		// Render recipe to view
-		recipeView.render(modelState.state.recipe);
-
-	} catch (err) {
-		addRecipe.renderError();
-	}
-}
+    // Render recipe to view
+    recipeView.render(modelState.state.recipe);
+  } catch (err) {
+    addRecipe.renderError();
+  }
+};
 
 // Handlers
 const init = function () {
-	addRecipe.addUploadHandler(controlUpload);
-	bookmarkView.addBookmarkHandler(conrolBookmark);
-	recipeView.addHandlerBookmarked(controlAddBookmark);
-	recipeView.addHandlerRender(controlRecipe);
-	searchView.addHandlerSearch(controlSearch);
-	paginationView.addHandlerPaganation(controlPaganation);
-	recipeView.addHandlerUpdateServings(controlServings);
+  addRecipe.addUploadHandler(controlUpload);
+  bookmarkView.addBookmarkHandler(conrolBookmark);
+  recipeView.addHandlerBookmarked(controlAddBookmark);
+  recipeView.addHandlerRender(controlRecipe);
+  searchView.addHandlerSearch(controlSearch);
+  paginationView.addHandlerPaganation(controlPaganation);
+  recipeView.addHandlerUpdateServings(controlServings);
 };
 init();
--- src/js/views/searchResult.js
+++ src/js/views/searchResult.js
@@ -3,15 +3,49 @@ import icons from 'url:../../img/icons.svg';
 import previewView from './PreviewView';
 
 class SearchResult extends View {
-	_parentEl = document.querySelector('.results');
-	_data;
-	_errorMessage = `We could not find the recipe based on your query 🙁`;
+  _parentEl = document.querySelector('.results');
+  _data;
+  _closeSidebarBtn = document.querySelector('.btn--close-sidebar');
+  _openSidebarBtn = document.querySelector('.sidebar-btn');
+  _errorMessage = `We could not find the recipe based on your query 🙁`;
+  _sideBar = document.querySelector('.search-results');
 
-	
-	_genereteMarkup() {
-		return this._data.map(result => previewView.render(result, false)).join(' ');
-	}
+  constructor() {
+    super();
+    this.toggleSideBar();
+  }
 
+  _genereteMarkup() {
+    return this._data
+      .map(result => previewView.render(result, false))
+      .join(' ');
+  }
+
+  toggleSideBar() {
+    [this._closeSidebarBtn, this._openSidebarBtn].forEach(btn => {
+      btn.addEventListener('click', this._toggleClass.bind(this));
+    });
+  }
+
+  _toggleClass() {
+    this._sideBar.classList.toggle('sidebar');
+  }
+
+  // _openRecipe() {
+  //   this._parentEl.addEventListener('click', function (e) {
+  //     const recipe = e.target.closest('.preview__link');
+  //     if (!recipe) return;
+  //   });
+
+  addHandlerB() {
+    this._parentEl.addEventListener('click', this._openRecipe.bind(this));
+  }
+
+  _openRecipe(e) {
+    const recipe = e.target.closest('.preview__link');
+    if (!recipe) return;
+    this._toggleClass();
+  }
 }
 
-export default new SearchResult;
+export default new SearchResult();
